c04be81dac4abce0f2080b89b025ee279530661f
Sebastián <sam171990@gmail.com>
2023-09-08 20:26:41 -0300

    Two chars added A B

--- main.py
+++ main.py
@@ -18,24 +18,22 @@ palm_points = [0,1,2,5,9,13,17]
 fingertips_points = [8,12,16,20]
 finger_base_points = [6,10,14,18]
 
-def hand_centroid(list):
-    print(list)
-    cord = np.array(list)
+def hand_centroid(cord_list):
+    cord = np.array(cord_list)
     centroid = np.mean(cord, axis=0)
-    print(centroid)
     centroid = int(centroid[0]), int(centroid[1])
     return centroid
 
 def finger_distance(base, tip, centx, centy):
     center = np.array([centx, centy])
-    l1 = np.linalg.norm(base - center, axis=1)
-    l2 = np.linalg.norm(tip - center, axis=1)
-    if l1 > l2:
+    dist_base = np.linalg.norm(center - base, axis=0)
+    dist_tip = np.linalg.norm(center - tip, axis=0)
+    if dist_base < dist_tip:
         return True
-    elif l2 > l1:
+    elif dist_tip < dist_base:
         return False
 
-def coordinates_calculator(thump, landmark, points, w, h):
+def coordinates_calculator(thump, landmark, points, w, h, hand_coord):
     coordinates = []
     for index in points:
         x = int(landmark.landmark[index].x * w)
@@ -51,10 +49,12 @@ def coordinates_calculator(thump, landmark, points, w, h):
     l3 = np.linalg.norm(p1 - p2)
     # Finger angle calculation
     if thump:
-        angle = degrees(acos((l1**2 + l3**2 - l2**2) / (2 * l1 *l3)))
-        return angle
+        cosine_calc = (l1**2 + l3**2 - l2**2) / (2 * l1 *l3)
+        if cosine_calc >= -1 and cosine_calc <= 1:
+            angle = degrees(acos(cosine_calc))
+            return angle
     else:
-        x,y = hand_centroid(palm_points)
+        x,y = hand_centroid(hand_coord)
         return finger_distance(p2, p1, x, y)
 
 
@@ -72,14 +72,20 @@ with mp_hands.Hands(
         height, width,_ = frame.shape
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         result = hands.process(frame_rgb)
+        hand_coordinates = []
+
 
         if result.multi_hand_landmarks:
             for hand_landmarks in result.multi_hand_landmarks:
-                thump_coordinates = coordinates_calculator(True, hand_landmarks, thump_points, width, height)
-                index_coordinates = coordinates_calculator(False, hand_landmarks, index_points, width, height)
-                middle_coordinates = coordinates_calculator(False, hand_landmarks, middle_points, width, height)
-                ring_coordinates = coordinates_calculator(False, hand_landmarks, ring_points, width, height)
-                little_coordinates = coordinates_calculator(False, hand_landmarks, little_points, width, height)
+                for index in palm_points:
+                    x = int(hand_landmarks.landmark[index].x * width)
+                    y = int(hand_landmarks.landmark[index].y * height)
+                    hand_coordinates.append([x, y])
+                thump_coordinates = coordinates_calculator(True, hand_landmarks, thump_points, width, height, hand_coordinates)
+                index_coordinates = coordinates_calculator(False, hand_landmarks, index_points, width, height, hand_coordinates)
+                middle_coordinates = coordinates_calculator(False, hand_landmarks, middle_points, width, height, hand_coordinates)
+                ring_coordinates = coordinates_calculator(False, hand_landmarks, ring_points, width, height, hand_coordinates)
+                little_coordinates = coordinates_calculator(False, hand_landmarks, little_points, width, height, hand_coordinates)
                 
                 mp_drawing.draw_landmarks(
                     frame,
@@ -88,14 +94,16 @@ with mp_hands.Hands(
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style()
                 )
-                if thump_coordinates in range(142, 154) and index_coordinates and middle_coordinates and ring_coordinates and little_coordinates:
+                if int(thump_coordinates) in range(120, 160) and index_coordinates and middle_coordinates and ring_coordinates and little_coordinates:
                     print('A')
+                if int(thump_coordinates) in range(120, 160) and not index_coordinates and not middle_coordinates and not ring_coordinates and not little_coordinates:
+                    print('B')
 
-                print(f'Thump: {thump_coordinates}')
-                print(f'Index: {index_coordinates}')
-                print(f'Middle: {middle_coordinates}')
-                print(f'Ring: {ring_coordinates}')
-                print(f'Little: {little_coordinates}')
+                # print(f'Thump: {thump_coordinates}')
+                # print(f'Index: {index_coordinates}')
+                # print(f'Middle: {middle_coordinates}')
+                # print(f'Ring: {ring_coordinates}')
+                # print(f'Little: {little_coordinates}')
         
         cv2.imshow("Video capture", frame)
         if cv2.waitKey(1) == ord('q'):
